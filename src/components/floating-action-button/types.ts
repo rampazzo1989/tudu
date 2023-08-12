37071b1d92fce5d4ae2ac7ab5b2a72a4191a1ffb
Felipe Rampazzo <felipe.farias@gazin.com.br>
2023-08-11 21:38:58 -0300

    feat: some more floating action development

--- src/components/floating-action-button/types.ts
+++ src/components/floating-action-button/types.ts
@@ -1,1 +1,18 @@
-export type FloatingActionButtonProps = {};
+import {
+  AnimatedIconProps,
+  BaseAnimatedIconRef,
+} from '../animated-icons/animated-icon/types';
+
+export type FloatingActionButtonProps = {
+  DefaultIcon: React.ForwardRefExoticComponent<
+    AnimatedIconProps & React.RefAttributes<BaseAnimatedIconRef>
+  >;
+};
+
+export type FloatingActionButtonRef = {
+  animateThisIcon: (
+    Icon: React.ForwardRefExoticComponent<
+      AnimatedIconProps & React.RefAttributes<BaseAnimatedIconRef>
+    >,
+  ) => void;
+};
