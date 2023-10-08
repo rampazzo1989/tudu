3df97da2cd5addf2499cf55cece9ba17b3acbfb8
Felipe Rampazzo <felipe.farias@gazin.com.br>
2023-10-08 20:05:54 -0300

    feat: close open swipeable when another one is opened

---
+++ src/hooks/useControlSwipeableState.tsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useCallback} from 'react';
+import {Swipeable} from 'react-native-gesture-handler';
+import {currentlyOpenSwipeableRef} from '../state/atoms';
+import {useRecoilState} from 'recoil';
+
+const useControlSwipeableState = (swipeableRef: React.RefObject<Swipeable>) => {
+  const [currentlyOpenSwipeable, setCurrentlyOpenSwipeable] = useRecoilState(currentlyOpenSwipeableRef);
+
+  const setOpenSwipeable = useCallback(() => {
+    setCurrentlyOpenSwipeable(swipeableRef);
+  }, [setCurrentlyOpenSwipeable, swipeableRef]);
+
+  useEffect(() => {
+    if (currentlyOpenSwipeable?.current !== swipeableRef.current) {
+      if (swipeableRef.current?.state.rowState !== 0) {
+        swipeableRef.current?.close();
+      }
+    }
+  }, [swipeableRef, currentlyOpenSwipeable]);
+
+  return {setOpenSwipeable};
+};
+
+export {useControlSwipeableState};
